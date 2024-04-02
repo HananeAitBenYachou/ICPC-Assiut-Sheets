31e4d85917974ac1f0dc36009677d5177ef5a18b
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-04-02 01:21:49 +0000

    added problem A. Say Hello With C++.cpp - https://codeforces.com/group/Bu2yXAqHyK/contest/463317/problem/A

---
+++ Newcomers-Training/Week-1/Sheet #1/A. Say Hello With C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+    cout << "Hanab";
+
+
+    int intValue;
+    long long longValue;
+    char charValue;
+    float floatValue;
+    double doubleValue;
+
+    cin >> intValue >> " " >> longValue >> " " >> charValue >> " " >> floatValue >> " " >> doubleValue;
+
+    cout << intValue << "\n" << longValue << "\n" << charValue << "\n" << floatValue << "\n" << doubleValue;
+    
+
+    return 0;
+}
