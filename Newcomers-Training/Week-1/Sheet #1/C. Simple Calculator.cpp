eff8467c683fbaf459cfdc2add618e9d3fd58caf
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-04-02 01:58:21 +0000

    added problem C. Simple Calculator - https://codeforces.com/group/Bu2yXAqHyK/contest/463317/problem/C

---
+++ Newcomers-Training/Week-1/Sheet #1/C. Simple Calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+    long long number1 , number2;
+
+    cin >> number1 >> number2;
+
+    cout << number1 << " + " << number2 << " = " << number1 + number2 << "\n";
+    cout << number1 << " * " << number2 << " = " << number1 * number2 << "\n";
+    cout << number1 << " - " << number2 << " = " << number1 - number2 << "\n";
+
+    return 0;
+}
