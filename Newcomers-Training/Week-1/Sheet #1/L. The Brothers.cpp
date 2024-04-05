72d3134bd0ff507bff8014887cb6c69382040e06
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-04-05 23:06:01 +0000

    added problem L. The Brothers - https://codeforces.com/group/Bu2yXAqHyK/contest/463317/problem/L

---
+++ Newcomers-Training/Week-1/Sheet #1/L. The Brothers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+	string name_1, name_2;
+
+	getline(cin, name_1);
+	getline(cin, name_2);
+
+	string lastName_1, lastName_2;
+
+	lastName_1 = name_1.substr(name_1.find(' ', 0) + 1);
+	lastName_2 = name_2.substr(name_2.find(' ', 0) + 1);
+
+	(lastName_1 == lastName_2) ? cout << "ARE Brothers" : cout << "NOT";
+
+	return 0;
+}
