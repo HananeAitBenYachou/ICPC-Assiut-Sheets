df9b697e1511d2a76e451fb57b9c293787ecbade
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-04-06 17:33:56 +0000

    added problem O. Calculator - https://codeforces.com/group/Bu2yXAqHyK/contest/463317/problem/O

---
+++ Newcomers-Training/Week-1/Sheet #1/O. Calculator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+	char operation;
+	
+	cin >> num1 >> operation >> num2;
+ 
+	switch (operation)
+	{
+		case '+':
+			cout << num1 + num2;
+			break;
+
+		case '-':
+			cout << num1 - num2;
+			break;
+
+		case '*':
+			cout << num1 * num2;
+			break;
+
+		case '/':
+			cout << num1 / num2;
+			break;
+	}
+	return 0;
+}
