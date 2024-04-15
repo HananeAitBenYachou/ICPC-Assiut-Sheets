3a9ef19caec27f40ee75c00c1e3932f46c684028
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-04-15 04:17:29 +0100

    added problem A. 1 to N - https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/A

---
+++ Newcomers-Training/Sheet #2 (Loops)/A. 1 to N.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    short n;
+
+    cin >> n; 
+
+    for (short i = 1; i <= n; i++)
+    {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
