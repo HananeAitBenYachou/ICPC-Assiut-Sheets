7e7c0ea9eb7423a719f29f50d74fd6a52552ea6b
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-04-23 00:52:12 +0100

    added problem B. Drawing 'X' - https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/B

---
+++ Newcomers-Training/Contest #2/B. Drawing 'X'.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	short n;
+	cin >> n;
+
+	for (short i = 0; i < n; i++)
+	{
+		for (short j = 0; j < n; j++)
+		{
+			if (i == (n / 2) && j == (n / 2))
+				cout << "X";
+
+			else if (i == j)
+				cout << "\\";
+				
+			else if (i == n - j - 1)
+				cout << "\/";
+
+			else
+				cout << "*";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
