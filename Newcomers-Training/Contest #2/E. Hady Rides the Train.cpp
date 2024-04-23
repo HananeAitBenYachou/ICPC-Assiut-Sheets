fe1bdff3184b3090ba725ac2bfc45aafba7c1a76
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-04-24 00:06:23 +0100

    added problem E. Hady Rides the Train - https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/E

---
+++ Newcomers-Training/Contest #2/E. Hady Rides the Train.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{	
+	long long id;
+	cin >> id;
+
+	long long rowNumber = id / 4;
+	short columnNumber = id % 4;
+
+	if (rowNumber % 2 != 0)	
+		columnNumber = 3 - columnNumber;
+
+	cout << rowNumber << " " << columnNumber;
+
+	return 0;
+}
