ea53ec5531f21b977e948040f98b303310e08b5d
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-04-23 21:15:34 +0100

    added problem D. Range Sum - https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/D

---
+++ Newcomers-Training/Contest #2/D. Range Sum.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+static long long GetSumOfNumbersInRange(long long l, long long r)
+{
+	long long minNumber = min(l, r);
+	long long maxNumber = max(l, r);
+
+	return ((maxNumber - minNumber + 1) * (minNumber + maxNumber)) / 2;
+}
+
+int main()
+{
+	int testCases = 0;
+	cin >> testCases;
+
+	long long l , r;
+
+	while (testCases-- > 0)
+	{
+		cin >> l >> r;
+		cout << GetSumOfNumbersInRange(l, r) << "\n";
+	}
+
+	return 0;
+}
