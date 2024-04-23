4e32bbdfd4ca9e709bed34ffe29a9950794f93d0
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-04-23 19:20:27 +0100

    added problem C. Finding Minimums - https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/C

---
+++ Newcomers-Training/Contest #2/C. Finding Minimums.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	long long number;
+	long long minimum = LLONG_MAX;
+
+	int counter = 0;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> number;
+
+		minimum = min(number, minimum);
+
+		if (++counter == k || i == n)
+		{
+			cout << minimum << " ";
+
+			minimum = LLONG_MAX;
+			counter = 0;
+		}
+	}
+
+	return 0;
+}
