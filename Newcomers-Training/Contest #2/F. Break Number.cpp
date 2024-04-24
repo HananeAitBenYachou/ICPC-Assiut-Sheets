74ce1109b957389496949d20ef9e1f1483092e90
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-04-24 01:20:22 +0100

    added problem F. Break Number - https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/F

---
+++ Newcomers-Training/Contest #2/F. Break Number.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+static long CountDivisionsBy2(long long number)
+{
+	long counter = 0;
+
+	while ((number & 1) == 0)
+	{
+		number >>= 1;
+		counter++;
+	}
+
+	return counter;
+}
+
+
+int main()
+{	
+	long n;
+	cin >> n;
+
+	long long number;
+	long maxTimes = 0;
+
+	while (n-- > 0)
+	{
+		cin >> number;
+		maxTimes = max(maxTimes, CountDivisionsBy2(number));
+	}
+
+	cout << maxTimes;
+
+	return 0;
+}
