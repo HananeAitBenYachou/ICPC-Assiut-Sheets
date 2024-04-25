59ba95292f2f2d469002e54af539d8c10034c759
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-04-25 03:17:24 +0100

    added problem H. Simple Mod - https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/H

---
+++ Newcomers-Training/Contest #2/H. Simple Mod.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+static pair<long long , long long> findValues(long long N)
+{
+    for (long long i = 0; i <= 1e9; ++i)
+    {
+        for (long long y = 0; y <= 1e9; ++y)
+        {
+            if ((i * i + y * y) % N == 0)            
+                return { i, y };          
+        }
+    }
+    return { -1, -1 };
+}
+
+int main() 
+{
+    long long N;
+    cin >> N;
+
+    pair<int, int> values = findValues(N);
+
+    if (values.first != -1 && values.second != -1) 
+        cout << values.first << " " << values.second << endl;
+    
+    else    
+        cout << "No solutions" << endl;
+    
+    return 0;
+}
